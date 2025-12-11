43aa90bace876728c9dda94e3b1987cd656699f1
James Linden <kodekrash@gmail.com>
2025-12-10 19:40:45 -0500

    update

--- src/PubSub.php
+++ src/PubSub.php
@@ -22,7 +22,7 @@ class PubSub extends Module {
         return (bool)count($topic->publish((new MessageBuilder())->setData(json_encode($data))->build()));
     }
 
-    public static function decodeData(object $data) : object {
+    public function decodeData(object $data) : object {
         return isset($data->message->data) ? json_decode(base64_decode($data->message->data)) : (object)[];
     }
 
