84f0ca00fa1c3197d27069f4117b2a9131cf9b1f
Dan Muntz <dan@wd4dan.net>
2026-01-04 12:48:38 -0500

    added comments for sync vs async

--- src/Logging.php
+++ src/Logging.php
@@ -14,16 +14,24 @@ class Logging extends Module {
         $lc = new LoggingClient();
         $this->source = $_SERVER['K_SERVICE'] ?? 'UNKNOWN_SOURCE';
 
-        // unbatched logging - logs will appear as they are emitted
+        // unbatched (sync) logging - logs will appear as they are emitted
+        // benefit:
+        //      logging entries will appear as they are sent, before script return
+        // side-effects:
+        //      increased RPC latency for each logging call
         $this->client = $lc->psrLogger($this->source);
 
-        // batched logging - logs will appear when the script returns
+        // batched (async) logging - logs will appear when the script returns
+        // benefit:
+        //      decreased RPC latency during script execution
+        // side-effects:
+        //      all emitted logs will only appear after script execution
         //$this->client = LoggingClient::psrBatchLogger($this->source);
     }
 
-    public function __call(string $name, array $arguments) : void {
+    public function __call(string $loglevel, array $arguments) : void {
         try {
-            $this->client->$name($this->source . ": " . $arguments[0]);
+            $this->client->$loglevel($this->source . ": " . $arguments[0]);
         } catch (\Throwable $e) {
             $this->instance->stderr->error("bolt::logger " . $e->getMessage());
         }
